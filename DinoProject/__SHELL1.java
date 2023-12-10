72a9ce89c27ee8046115b3c4c03bfa1e5cf7579e
shewiee <shmaealivio@gmail.com>
2023-12-10 10:52:28 +0800

    Java Form
    netbeans

---
+++ DinoProject/__SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+
+try {
+return makeObj(java.awt.Frame.getFrames()
+);}
+finally {
+}
+}}
